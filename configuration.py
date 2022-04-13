0bdde1d589d76c18a3c77b6c6cd9d9165164f65a
Thandiwe-Khalaki <thandily@gmail.com>
2022-04-13 17:36:11 +0200

    added new files

---
+++ configuration.py
@@ -0,0 +1,22 @@
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
+
+SCOPES = ["https://www.googleapis.com/auth/sqlservice.admin"]
+SERVICE_ACCOUNT_FILE = "sita-automation-ba053c79803c.json"
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
+SPREADSHEET_ID_2021 = "1GqeQ4kT79AizP6Wfr29ggvB8-ND4yPghwaA-BljUXPc"
+SPREADSHEET_ID_2022 = "1JPZBtnOPuWU3uiBA7QUXujH-6UFO_ndzN2t01RBDx7E"
+creds = None
+creds = service_account.Credentials.from_service_account_file(
+    SERVICE_ACCOUNT_FILE, scopes=SCOPES
+)
+
+service = build("sheets", "v4", credentials=creds)
+sheet = service.spreadsheets()
+result = (
+    sheet.values()
+    .get(spreadsheetId=SPREADSHEET_ID_2021, range="Cohorts!A1:V12")
+    .execute()
+)
+print(result)
+#values = result.get("values", [])
