7be95b52803508b1f45c85eee65ff4949d71ae8d
TrevorKS9 <makhiler@gmail.com>
2022-04-21 13:18:48 +0200

    Create read.py

---
+++ read.py
@@ -0,0 +1,37 @@
+# GOOGLE API for CODE
+# extract data on google
+
+from google.oauth2 import service_account
+
+from googleapiclient.discovery import build
+
+
+
+from google.oauth2 import service_account
+
+
+SERVICE_ACCOUNT_FILE = 'key.json'
+
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+
+creds=None
+creds = service_account.Credentials.from_service_account_file(
+        SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+
+
+# The ID and range of a sample spreadsheet.
+SAMPLE_SPREADSHEET_ID = '1NFpUsiy9eGmI-qPEMqHgHAyIKFQVW_trVB7i15x2IvQ'
+
+service = build('sheets', 'v4', credentials=creds)
+
+# Call the Sheets API
+sheet = service.spreadsheets()
+result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                        range="Cohorts!A1:H13").execute()
+values = result.get('values', [])
+
+print(values)
+
+
+
+
